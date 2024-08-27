0d2a0a6a32b3e874173d145636ba10e8bbc82ac4
AdmiN <AdmiN@ITWS067>
2024-08-27 10:15:46 +0530

    commit

--- src/welcome.java
+++ src/welcome.java
@@ -3,7 +3,7 @@ public class welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to devops lab");
+		System.out.println("welcome to devops ci/cd lab");
 
 	}
 
